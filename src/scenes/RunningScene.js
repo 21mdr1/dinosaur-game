138a34ada1a40ed3b4a608994279874c2c4e54aa
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-04-09 17:19:39 -0400

    Write game ^Cene boilerplate

---
+++ src/scenes/RunningScene.js
@@ -0,0 +1,15 @@
+import Phaser from 'phaser';
+
+export default class RunningScene extends Phaser.Scene {
+    constructor() {
+        super('running-scene');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+}
